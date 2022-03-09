3f5ddc01914f2f525c9131787ada3b228a1c0ee7
G.R.S.S Jayaratne <sjayarathne@i-context.net>
2022-03-09 21:41:15 +0530

    initial commit

---
+++ GrpcClientApp/GrpcClientApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using Grpc.Net.Client;
+using System;
+using Grpc.Core;
+using System.Threading.Tasks;
+
+namespace GrpcClientApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello Grpc from dev!");
+
+            //channel to communicate with the service
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+
+            var customerClient = new Customer.CustomerClient(channel);
+
+            var result = await customerClient.GetCustomerInfoAsync(new CustomerFindModel()
+            {
+                UserId = 2
+            });
+
+            Console.WriteLine($"First Name{result.FirstName} \nLastName{result.LastName}");
+
+            var allCustomers = customerClient.GetAllCustomers(new AllCustomerModel());
+
+            await foreach (var customer in allCustomers.ResponseStream.ReadAllAsync())
+            {
+                Console.WriteLine($"{customer.FirstName} {customer.LastName}");
+            }
+        }
+    }
+}
---
+++ GrpcService/GrpcService/Services/CustomerService.cs
@@ -0,0 +1,79 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcService.Services
+{
+    public class CustomerService:Customer.CustomerBase
+    {
+        private readonly ILogger<CustomerService> _logger;
+        public CustomerService(ILogger<CustomerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerDataModel> GetCustomerInfo(CustomerFindModel request, ServerCallContext context)
+        {
+            var model = new CustomerDataModel();
+            if(request.UserId==1)
+            {
+                model.FirstName = "shakila";
+                model.LastName = "sameera";
+            }
+            else if(request.UserId == 2)
+            {
+                model.FirstName = "namal";
+                model.LastName = "nissanka";
+            }
+            else if (request.UserId == 3)
+            {
+                model.FirstName = "bruce";
+                model.LastName = "wayne";
+            }
+            else if (request.UserId == 2)
+            {
+                model.FirstName = "micky";
+                model.LastName = "mouce";
+            }
+
+            return Task.FromResult(model);
+        }
+
+
+
+        public override async Task GetAllCustomers(AllCustomerModel request,IServerStreamWriter<CustomerDataModel> responseStream, ServerCallContext context)
+        {
+            var allCustomers = new List<CustomerDataModel>();
+
+            var c1 = new CustomerDataModel();
+            c1.FirstName = "Bruce";
+            c1.LastName = "Wayne";
+            allCustomers.Add(c1);
+
+            var c2 = new CustomerDataModel();
+            c2.FirstName = "Homer";
+            c2.LastName = "Simpsons";
+            allCustomers.Add(c2);
+
+            var c3 = new CustomerDataModel();
+            c3.FirstName = "Tony";
+            c3.LastName = "Stark";
+            allCustomers.Add(c3);
+
+            var c4 = new CustomerDataModel();
+            c4.FirstName = "Mickey";
+            c4.LastName = "Mouse";
+            allCustomers.Add(c4);
+
+            foreach (var item in allCustomers)
+            {
+                await responseStream.WriteAsync(item);
+
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
